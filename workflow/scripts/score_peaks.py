4ed3a42dbed9f9cc1112079046ebb7be944749f9
Austin <austin.wang1357@gmail.com>
2022-10-06 17:20:00 -0700

    debug

--- workflow/scripts/score_peaks.py
+++ workflow/scripts/score_peaks.py
@@ -75,6 +75,9 @@ def main(ss_paths, xs_paths, out_path):
         "-log10p": nlp,
         "-log10q": nlq
     }
+    for k, v in cols.items(): ####
+        print(k)
+        print(v) 
     table = pd.DataFrame(cols)
     table.sort_values("-log10q")
 
